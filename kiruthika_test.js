635c1e98f07e8f835b0d87eba08fd8515ddc6b50
kiga0001 <kiruthika.ganeshan0001@stud.hkr.se>
2025-12-17 19:25:33 +0100

    variables practice

--- kiruthika_test.js
+++ kiruthika_test.js
@@ -1,1 +1,82 @@
 // this file is dedicated to kiruthika
+//parselnt- converts string to a integer
+//variables and operations
+/*
+let num = 8;
+let str = "kiruthika";
+console.log(str)
+console.log(num)
+*/
+/*
+let name = "kiruthika";
+let age = 28;
+let gender = "female";
+console.log(name)
+console.log(age)
+console.log(gender)
+console.log(`My name is ${name} and my age is ${age}`);
+*/
+/*
+let number = 20;
+let str = "10";
+let result = number + str;
+console.log(result);
+
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log(c)
+/*
+
+/*  Variables, Operations and Expressions
+let a = 5;
+let b = 3;
+let c = a + b;
+console.log(c); 
+*/
+
+//Tasks
+//let num = 42;
+//console.log(num);
+
+//Convert num into a string and store it in a new variable called strNum
+//let a = 10;
+//let b = "3";
+//let c = a+b;
+//console.log(c)
+//Display the result, and ensure that strNum is indeed a string using 
+
+//let a = 42;
+//let strnum = "42";
+//console.log(a+strnum)
+
+
+// Task 2: Working with Arrays
+//let fruits = ["apple", "orange", "banana"];
+//console.log(fruits)
+
+//fruits.push("pineapple");
+//console.log(fruits)
+
+//fruits.pop()
+//console.log(fruits)
+
+//console.log(fruits[0])
+//console.log(fruits[1])
+//console.log(fruits[2])
+//let lengthOfArray = fruits.length;
+//console.log("The array has", lengthOfArray, "fruits.");
+
+/*let person = {
+  name: "Kiruthika",
+  age: 28,
+  email: "kiruthika.test.com"
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.email); 
+*/
+
+
+
